526aa099a4bcbeda3f3e3c7e2335ddcc7948b4f8
billythegoat101 <billythegoat101@gmail.com>
2013-03-06 09:37:37 -0500

    Void inscription implemented

--- minecraft/dustmodtestpack/inscriptions/VoidInscription.java
+++ minecraft/dustmodtestpack/inscriptions/VoidInscription.java
@@ -1,7 +1,9 @@
 package dustmodtestpack.inscriptions;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import dustmod.DustEvent;
 import dustmod.EntityDust;
@@ -17,13 +19,17 @@ public class VoidInscription extends InscriptionEvent {
 		this.setDescription("Description:\n"
 				+ "Automatically store all picked-up items into the void. Use the Void Storage rune to summon them back.");
 		this.setNotes("Sacrifice:\n" +
-				"TBD");
+				"-4xObsidianBlock + 2xEnderPearl + 5XP");
 	}
-
+	
 	@Override
 	public boolean callSacrifice(DustEvent rune, EntityDust e, ItemStack item) {
-		// TODO Auto-generated method stub
-		return super.callSacrifice(rune, e, item);
+		ItemStack[] req = new ItemStack[]{new ItemStack(Block.obsidian,4), new ItemStack(Item.enderPearl, 2)};
+		req = rune.sacrifice(e, req);
+		if(!rune.checkSacrifice(req)) return false;
+		if(!rune.takeXP(e, 5));
+		item.setItemDamage(0);
+		return true;
 	}
 
 	@Override
